ee7af9bd5f18a10abcb5a9c1581f869a8cab773c
elaaasri <anas_elasri@outlook.fr>
2024-12-23 18:47:27 +0100

    add randomLetterEffect(), func to apply random letter effect for a text

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mobileNavContainer = document.getElementById("mobile-nav-container");
 const navOverlayWindow = document.getElementById("nav-overlay-window");
 const mobileNavIcon = document.getElementById("mobile-nav-icon");
+const letterEffectText = document.getElementById("letter-effect-text");
 // const viewportWidth = window.innerWidth;
 
 // show mobile container :
@@ -25,3 +26,29 @@ const hideMobileNavContainer = () => {
   );
 };
 navOverlayWindow.addEventListener("click", hideMobileNavContainer);
+
+// random letter effect animation func :
+const randomLetterEffect = (element) => {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#%&^+=-.";
+  const targetText = "Hello.";
+  let currentIndex = 0;
+  const displayLetter = () => {
+    let id = setInterval(() => {
+      const randomLetter =
+        characters[Math.floor(Math.random() * characters.length)];
+      element.textContent =
+        element.textContent.slice(0, currentIndex) + randomLetter;
+      if (randomLetter == targetText[currentIndex]) {
+        clearInterval(id);
+        element.textContent =
+          element.textContent.slice(0, currentIndex) + targetText[currentIndex];
+        currentIndex++;
+      }
+    }, 30);
+  };
+  for (let i = 0; i < targetText.length; i++) {
+    displayLetter();
+  }
+};
+randomLetterEffect(letterEffectText);
